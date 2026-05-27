1de2dc10839fc1df00bb22dc0ca33972d6377258
MateoEspinoza23 <20233495@aloe.ulima.edu.pe>
2026-05-27 15:20:13 -0500

    1ER AVANCE DE ASISTENCIA

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './componentes/MainLayout';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './componentes/Home';
+import Asistencia from './componentes/Asistencia';
 import Login from './componentes/LoginUsuario';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Home />} />
+          <Route path="/Asistencia" element={<Asistencia />} />
         
           <Route path="/LoginUsuario" element={<Login />}/>
 
