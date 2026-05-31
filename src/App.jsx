f066fa98208891c809a9d66ddfcf87e52ee0d6c5
yamyl <yamylflores@gmail.com>
2026-05-30 23:01:18 -0500

    Conectar perfil con la variable context AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Home from './componentes/Home';
 import Login from './componentes/LoginUsuario';
 import Asistencia from './componentes/Asistencia';
 import RegistrarUsuario from './componentes/RegistrarUsuario';
+import Perfil from './componentes/Angela/Perfil';
 
 // Componentes desarrollados por Daniel Sandoval
 import ReservaCheckout from './pages/Reservas/ReservaCheckout';
@@ -32,6 +33,7 @@ function App() {
           <Route path="/Reserva/:destino" element={<Reserva />} />
           <Route path="/Eventos" element={<Eventos />} />
           <Route path="/admin/lugares" element={<AdminLugares />} />
+          <Route path="/perfil" element={<Perfil />} />
 
         </Routes>
       </MainLayout>
